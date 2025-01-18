f7ebd71ccc87d3164bd40da7d9a1c990412118f8
Максим Иванов <omaxphp@yandex.ru>
2025-01-18 23:19:32 +0300

    Create en.js

---
+++ javascript/hexlet/i18n/en.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    russian: 'Русский',
+    english: 'English',
+    clicks_zero: '{{count}} clicks',
+    clicks_one: '{{count}} clicks',
+    clicks_few: '{{count}} clicks',
+    clicks_many: '{{count}} clicks',
+    clicks_other: '{{count}} clicks',
+    reset: 'Reset'
+  },
+};
