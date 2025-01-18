c8e570a45ca8e4c0f2d78263683c3f38eaba6a02
Максим Иванов <omaxphp@yandex.ru>
2025-01-18 23:19:18 +0300

    Create ru.js

---
+++ javascript/hexlet/i18n/ru.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    russian: 'Русский',
+    english: 'English',
+    clicks_zero: '{{count}} кликов',
+    clicks_one: '{{count}} клик',
+    clicks_few: '{{count}} клика',
+    clicks_many: '{{count}} кликов',
+    clicks_other: '{{count}} кликов',
+    reset: 'Сбросить'
+  },
+};
